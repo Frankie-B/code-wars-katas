169ba2cbb14f7c814bc6a9462acb75153cb44d94
Francis Bukenya <frankie.bukenya@gmail.com>
2020-03-05 12:22:36 +0100

    Add katas 5/4/20

---
+++ reference.js
@@ -0,0 +1,6 @@
+// References and information to remember when solving Katas.
+
+// Exclusive or or exclusive disjunction is a logical operation that outputs true only when inputs differ (one is true, the other is false)
+
+//The forEach() method executes a provided function once for each array element.
+// Syntax = arr.forEach(callback(currentValue [, index [, array]])[, thisArg])
