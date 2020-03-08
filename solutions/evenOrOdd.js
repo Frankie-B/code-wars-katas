8cae514b61289a03ae987a5266cd2f592fd1012c
Francis Bukenya <frankie.bukenya@gmail.com>
2020-03-08 21:05:17 +0100

    Add even or odd solution

---
+++ solutions/evenOrOdd.js
@@ -0,0 +1,11 @@
+//Even or Odd
+/**
+ *
+ *Create a function (or write a script in Shell) that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+ */
+
+function even_or_odd(number) {
+  return number % 2 === 0 ? 'Even' : 'Odd';
+}
+
+// Clever solution: const even_or_odd = number => number % 2 === 0 ? 'Even' : 'Odd';
