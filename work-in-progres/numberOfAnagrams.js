c11627492c88d1f77f589036b30aa342c36bc112
Francis Bukenya <frankie.bukenya@gmail.com>
2020-03-12 16:40:36 +0100

    Update kata solutions

---
+++ work-in-progres/numberOfAnagrams.js
@@ -0,0 +1,16 @@
+// Number of anagrams in an array of words - 6Kyu / https://www.codewars.com/kata/587e18b97a25e865530000d8/train/javascript
+
+/**
+ * An anagram is a word, a phrase, or a sentence formed from another by rearranging its letters. An example of this is "angel", which is an anagram of "glean".
+
+Write a function that receives an array of words, and returns the total number of distinct pairs of anagramic words inside it.
+
+Some examples:
+
+There are 2 anagrams in the array ["dell", "ledl", "abc", "cba"]
+There are 7 anagrams in the array ["dell", "ledl", "abc", "cba", "bca", "bac"]
+ */
+
+function anagramCounter(wordsArray) {
+  return counter;
+}
