c9903368454c93a0b118e1e420f831b4d1fdad6b
Francis Bukenya <frankie.bukenya@gmail.com>
2020-03-28 20:55:05 +0100

    Ádd kata solutions

--- work-in-progres/firstAndLastCharII.js
+++
@@ -1,3 +0,0 @@
-/*
-This is a spin off of my first kata. You are given a list of character sequences as a comma separated string. Write a function which returns another string containing all the character sequences except the first and the last ones. If the input string is empty, or the removal of the first and last items would cause the string to be empty, return a null value.
-*/
