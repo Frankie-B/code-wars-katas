b1a88855fb6bf0a37464136263ede6f771535bc2
Francis Bukenya <frankie.bukenya@gmail.com>
2020-03-31 09:04:21 +0200

    Add kata solutions

---
+++ edabit/solutions/areaTriangle.js
@@ -0,0 +1,13 @@
+// Area of a Triangle
+
+/**
+ * Write a function that takes the base and height of a triangle and return its area.
+ *
+ * The area of a triangle is: (base * height) / 2
+ */
+
+function triArea(base, height) {
+  return (base * height) / 2;
+}
+
+triArea(10, 6);
---
+++ edabit/solutions/equalNumbers.js
@@ -0,0 +1,11 @@
+// Are the Numbers Equal? - https://edabit.com/challenge/QSnaSH5S3oxZkwcNc
+
+/**
+ * Create a function that returns true when num1 is equal to num2; otherwise return false.
+ */
+
+function isSameNum(num1, num2) {
+  return num1 === num2 ? true : false;
+}
+
+isSameNum(2, 2);
