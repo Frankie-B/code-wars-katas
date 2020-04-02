a1f5f27f515fac5644d83970bc1ac0c9ccc402da
Francis Bukenya <frankie.bukenya@gmail.com>
2020-04-02 15:09:02 +0200

    Add leetCode day2

---
+++ leetCode/30DayChallenge/day2.js
@@ -0,0 +1,27 @@
+//
+
+/**
+ *Write an algorithm to determine if a number is "happy".
+
+A happy number is a number defined by the following process: Starting with any positive integer, replace the number by the sum of the squares of its digits, and repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1. Those numbers for which this process ends in 1 are happy numbers.
+ */
+
+var isHappy = function(n, counter = 0) {
+  result = false;
+  if (counter < 8) {
+    let arr = n
+      .toString()
+      .split('')
+      .map(n => n * n);
+    let sum = arr.reduce((acc, curr) => acc + curr, 0);
+    if (sum === 1) {
+      return (result = true);
+    } else {
+      counter++;
+      isHappy(sum, counter);
+    }
+  }
+  return result;
+};
+
+isHappy(19);
