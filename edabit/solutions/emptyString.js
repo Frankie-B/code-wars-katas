ed1b2cfd9278cb0df15ecb4e268c310e9e4f5ac8
Francis Bukenya <frankie.bukenya@gmail.com>
2020-04-04 07:38:56 +0200

    Add edabit solution

---
+++ edabit/solutions/emptyString.js
@@ -0,0 +1,11 @@
+// Is the String Empty? - https://edabit.com/challenge/EzbfiquDoAc2Zc9FL
+
+/**
+ *
+ *Create a function that returns true if a string is empty and false otherwise.
+ */
+
+function isEmpty(s) {
+  return s === '' ? true : false;
+}
+isEmpty('');
---
+++ edabit/wip/farmProblem.js
@@ -0,0 +1,16 @@
+// The Farm Problem - https://edabit.com/challenge/8Qg78sf5SNDEANKti
+
+/**
+ *In this challenge, a farmer is asking you to tell him how many legs can be counted among all his animals. The farmer breeds three species:
+
+chickens = 2 legs
+cows = 4 legs
+pigs = 4 legs
+The farmer has counted his animals and he gives you a subtotal for each species. You have to implement a function that returns the total number of legs of all the animals.
+ */
+
+function animals(chickens, cows, pigs) {
+  return chickens * 2 + cows * 4 + pigs * 4;
+}
+
+animals(2, 4, 5);
