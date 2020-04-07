39c75acdda367e855694a4d7d0255146086c76b8
Francis Bukenya <frankie.bukenya@gmail.com>
2020-04-07 14:36:16 +0200

    Add leetCode day6

---
+++ leetCode/30DayChallenge/day7.js
@@ -0,0 +1,21 @@
+// Counting Elements - https://leetcode.com/explore/featured/card/30-day-leetcoding-challenge/528/week-1/3289/
+
+/**
+ * Given an integer array arr, count element x such that x + 1 is also in arr.
+
+If there're duplicates in arr, count them seperately.
+ */
+
+var countElements = function (arr) {
+  let count = 0;
+  let arr2 = [...arr];
+  arr.forEach((n) => {
+    if (arr2.includes(n + 1)) {
+      count++;
+    }
+  });
+  return count;
+};
+
+countElements;
+[1, 2, 3];
