a3ec8a31b7553d88dd869b8e7221245a718466da
Francis Bukenya <frankie.bukenya@gmail.com>
2020-05-07 06:48:43 +0200

    Add new kata collection

---
+++ hackerRank/work-in-Progress/utopianTree.js
@@ -0,0 +1,33 @@
+// Utopian Tree - https://www.hackerrank.com/challenges/utopian-tree/problem?utm_campaign=challenge-recommendation&utm_medium=email&utm_source=7-day-campaign
+
+// The Utopian Tree goes through 2 cycles of growth every year. Each spring, it doubles in height. Each summer, its height increases by 1 meter.
+
+// Laura plants a Utopian Tree sapling with a height of 1 meter at the onset of spring. How tall will her tree be after
+
+// growth cycles?
+
+// For example, if the number of growth cycles is
+
+// , the calculations are as follows:
+
+// Period  Height
+// 0          1
+// 1          2
+// 2          3
+// 3          6
+// 4          7
+// 5          14
+
+// Function Description
+
+// Complete the utopianTree function in the editor below. It should return the integer height of the tree after the input number of growth cycles.
+
+// utopianTree has the following parameter(s):
+
+//     n: an integer, the number of growth cycles to simulate
+
+// Input Format
+
+// The first line contains an integer,
+// , the number of test cases.
+// subsequent lines each contain an integer, , denoting the number of cycles for that test case.
