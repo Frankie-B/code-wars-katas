b8f18507e82da1fdbb0a37abc5a54bb2f97dc1e7
Francis Bukenya <frankie.bukenya@gmail.com>
2020-05-09 16:27:48 +0200

    Add solution to map.js

--- algos/map.js
+++ algos/map.js
@@ -36,17 +36,64 @@ let f = e.map((v, i) => i);
 console.log(f);
 
 // map to nested, logs [[1, 2, 3], [1, 2, 3], [1, 2, 3]]
+let x = [
+  [1, 2, 3],
+  [1, 2, 3],
+  [1, 2, 3],
+];
+
+x.map((v) => v);
+console.log(x);
+
+var a = [{ name: 'john' }, { name: 'frank' }, { name: 'ron' }];
+
+// function addS(name) {
+//   const nameCapitalized = name.charAt(0).toUpperCase() + name.slice(1)
+//   return nameCapitalized + 's';
+// }
+
+// a.map((v, i) => v.value = addS(v.name));
+// console.log(a);
+
+// a.map(function(v) {
+//   v.value = v.name.charAt(0).toUpperCase() + v.name.slice(1) + 's';
+//   return v;
+// });
+
+a.map(
+  (v) => (v.value = v.name.charAt(0).toUpperCase() + v.name.slice(1) + 's')
+);
+
+console.log(a);
+
+// map to exactly this: [
+//   { name: 'john', number: 1 },
+//   { name: 'frank', number: 1 },
+//   { name: 'ron', number: 1 }
+// ]
 
 // var a = [
 //   { name: 'cats' },
 //   { name: 'dogs' },
 //   { name: 'tigers' },
-// ]
+// ];
+
+// a.map(v => v.name);
+// console.log(a);
+
+// a.map((v) => v)
+
+// console.log(a);
 
 // map to [{ name: 'cats', value: 'Cat' } etc
+// let y = a.map((v) => v.name.length);
+
+// console.log(y);
 
 // map to [{ name: {nameLength}, value: {valueLength} }, etc], logs [{ name: 4, value: 3 }, etc]
 
+// let z = y.map( v => value.length);
+// console.log(z);
 // map to total characters in object, logs [7, 7, 11]
 
 // combine the array's, log [ { numbersOfCats: 7, name: 'cats', value: '7 cats' }, etc]
