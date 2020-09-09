4beba0b73b202e7951bb223ef74952c85e933365
Frankie Bukenya <frankie.bukenya@gmail.com>
2020-09-09 11:15:13 +0200

    Add solution edabit

---
+++ edabit/solutions/bitwiseOperations.js
@@ -0,0 +1,15 @@
+// https://edabit.com/challenge/vvuAkYEAArrZvmp6X
+
+// Write three functions to calculate the bitwise AND, bitwise OR and bitwise XOR of two numbers.
+
+function bitwiseAND(n1, n2) {
+	return n1 & n2;
+}
+
+function bitwiseOR(n1, n2) {
+	return n1 | n2;
+}
+
+function bitwiseXOR(n1, n2) {
+	return n1 ^ n2;
+}
